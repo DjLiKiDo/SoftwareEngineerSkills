e73c209d3b9dec4e7fee14eb104ee1501b7e3265
DjLiKiDo <djlikido@gmail.com>
2025-05-19 22:36:26 +0200

    feat: Add InternalsVisibleTo attributes for Moq and unit tests

---
+++ SoftwareEngineerSkills/src/SoftwareEngineerSkills.Infrastructure/InternalsVisibleTo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+// Make internals visible to Moq
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("SoftwareEngineerSkills.Infrastructure.UnitTests")]
